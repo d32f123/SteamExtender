a3bc32a09bcaa08caa3dddbbb2d6f95508bb3f46
d32f123 <d32f123@yandex.ru>
2016-09-10 21:20:06 +0300

    Completed Graph

--- SteamExtender/MainWindow.xaml.cs
+++ SteamExtender/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using LiveCharts.Defaults;
 using System.Threading;
 using LiveCharts.Configurations;
 using System.ComponentModel;
+using LiveCharts.Definitions.Series;
 
 namespace SteamExtender
 {
@@ -26,7 +27,19 @@ namespace SteamExtender
     /// </summary>
     public partial class MainWindow : Window
     {
-        public SeriesCollection Series { get; set; }
+        public SeriesCollection Series2 { get; set; }
+
+        public enum SteamStatus //TODO: Перенести в отдельный класс
+        {
+            NaN = -1,
+            Offline,
+            Snooze,
+            Away,
+            Online,
+            InGame
+        }
+
+        public string[] Labels { get; set; }
 
         public class DateModel
         {
@@ -62,14 +75,14 @@ namespace SteamExtender
                 fullRes = value;
                 MinimumPoint = MinimumPoint - diffset / 2;
                 MaximumPoint = MaximumPoint + diffset / 2;
-
+                chart.AxisX[0].Separator.Step = (MaximumPoint - MinimumPoint) / NumberOfSteps;
             }
         }
         private double StepWidthInPixels // ширина шага в пикселях
         {
             get
             {
-                return Width / NumberOfSteps;
+                return chart.ActualWidth / NumberOfSteps;
             }
         }
         private double StepWidthInSeconds
@@ -93,8 +106,20 @@ namespace SteamExtender
             {
                 if (!chart.AxisX[0].MinValue.HasValue)
                 {
-                    chart.AxisX[0].MinValue = DoubleConverter.
-                        FromDateToGraph(((chart.Series[0] as StepLineSeries).Values[0] as DateModel).DateTime);
+                    double min = Double.MaxValue;
+                    for (int i = 0; i < 5; ++i)
+                    {
+                        if ((chart.Series[i] as LineSeries).Values.Count == 0)
+                            continue;
+                        if (DoubleConverter.
+                        FromDateToGraph(((chart.Series[i] as LineSeries).Values[0] as DateModel).DateTime) < min)
+                            min = DoubleConverter.
+                        FromDateToGraph(((chart.Series[i] as LineSeries).Values[0] as DateModel).DateTime);
+                    }
+                    if (min == Double.MaxValue)
+                        chart.AxisX[0].MinValue = 0.0;
+                    else
+                        chart.AxisX[0].MinValue = min;
                 }
                 return (double)chart.AxisX[0].MinValue;
             }
@@ -102,8 +127,20 @@ namespace SteamExtender
             {
                 if (value < 0)
                 {
-                    chart.AxisX[0].MinValue = DoubleConverter.
-                        FromDateToGraph(((chart.Series[0] as StepLineSeries).Values[0] as DateModel).DateTime);
+                    double min = Double.MaxValue;
+                    for (int i = 0; i < 5; ++i)
+                    {
+                        if ((chart.Series[i] as LineSeries).Values.Count == 0)
+                            continue;
+                        if (DoubleConverter.
+                        FromDateToGraph(((chart.Series[i] as LineSeries).Values[0] as DateModel).DateTime) < min)
+                            min = DoubleConverter.
+                        FromDateToGraph(((chart.Series[i] as LineSeries).Values[0] as DateModel).DateTime);
+                    }
+                    if (min == Double.MaxValue)
+                        chart.AxisX[0].MinValue = 0.0;
+                    else
+                        chart.AxisX[0].MinValue = min;
                     return;
                 }
                 chart.AxisX[0].MinValue = value;
@@ -116,7 +153,7 @@ namespace SteamExtender
             {
                 if (!chart.AxisX[0].MaxValue.HasValue)
                 {
-                    chart.AxisX[0].MaxValue = chart.AxisX[0].MaxValue = MinimumPoint + FullResolution;
+                    chart.AxisX[0].MaxValue = MinimumPoint + FullResolution;
                 }
                 return (double)chart.AxisX[0].MaxValue;
             }
@@ -133,7 +170,88 @@ namespace SteamExtender
 
         public LiveCharts.Configurations.CartesianMapper<DateModel> dayConfig;
 
-        private ChartValues<MainWindow.DateModel> observableValues;
+
+        private struct StatusValues
+        {
+            internal SteamStatus previousAddition;
+
+            public ChartValues<DateModel> OnlineValues { get; internal set; }
+            public ChartValues<DateModel> InGameValues { get; internal set; }
+            public ChartValues<DateModel> AwayValues { get; internal set; }
+            public ChartValues<DateModel> SnoozeValues { get; internal set; }
+            public ChartValues<DateModel> OfflineValues { get; internal set; }
+            
+            public void AddValue(DateTime Date, SteamStatus Status)
+            {
+                if (Status != previousAddition)
+                {
+                    switch (previousAddition)
+                    {
+                        case SteamStatus.InGame:
+                            InGameValues.Add(new DateModel { DateTime = Date, Value = 4 });
+                            InGameValues.Add(new DateModel { DateTime = Date, Value = Double.NaN });
+                            break;
+                        case SteamStatus.Online:
+                            OnlineValues.Add(new DateModel { DateTime = Date, Value = 3 });
+                            OnlineValues.Add(new DateModel { DateTime = Date, Value = Double.NaN });
+                            break;
+                        case SteamStatus.Away:
+                            AwayValues.Add(new DateModel { DateTime = Date, Value = 2 });
+                            AwayValues.Add(new DateModel { DateTime = Date, Value = Double.NaN });
+                            break;
+                        case SteamStatus.Snooze:
+                            SnoozeValues.Add(new DateModel { DateTime = Date, Value = 1 });
+                            SnoozeValues.Add(new DateModel { DateTime = Date, Value = Double.NaN });
+                            break;
+                        case SteamStatus.Offline:
+                            OfflineValues.Add(new DateModel { DateTime = Date, Value = 0 });
+                            OfflineValues.Add(new DateModel { DateTime = Date, Value = Double.NaN });
+                            break;
+                    }
+                    previousAddition = Status;
+                }
+                switch(Status)
+                {
+                    case SteamStatus.InGame:
+                        InGameValues.Add(new DateModel { DateTime = Date , Value = 4});
+                        break;
+                    case SteamStatus.Online:
+                        OnlineValues.Add(new DateModel { DateTime = Date, Value = 3 });
+                        break;
+                    case SteamStatus.Away:
+                        AwayValues.Add(new DateModel { DateTime = Date, Value = 2 });
+                        break;
+                    case SteamStatus.Snooze:
+                        SnoozeValues.Add(new DateModel { DateTime = Date, Value = 1 });
+                        break;
+                    case SteamStatus.Offline:
+                        OfflineValues.Add(new DateModel { DateTime = Date, Value = 0 });
+                        break;
+                }
+            }
+        }
+
+        private StatusValues observableValues1 = new StatusValues
+        {
+            previousAddition = SteamStatus.NaN,
+            OnlineValues = new ChartValues<DateModel>(),
+            InGameValues = new ChartValues<DateModel>(),
+            AwayValues = new ChartValues<DateModel>(),
+            SnoozeValues = new ChartValues<DateModel>(),
+            OfflineValues = new ChartValues<DateModel>()
+        };
+
+        private StatusValues observableValues2 = new StatusValues
+        {
+            previousAddition = SteamStatus.NaN,
+            OnlineValues = new ChartValues<DateModel>(),
+            InGameValues = new ChartValues<DateModel>(),
+            AwayValues = new ChartValues<DateModel>(),
+            SnoozeValues = new ChartValues<DateModel>(),
+            OfflineValues = new ChartValues<DateModel>()
+        };
+
+
 
         private Point currMousePosition { get; set; }
         
@@ -146,7 +264,14 @@ namespace SteamExtender
             InitArray();
             DataContext = this;
             currMousePosition = new Point(-1, -1);
-            
+            Labels = new string[]
+            {
+                "Offline",
+                "Snooze",
+                "Away",
+                "Online",
+                "In-Game",
+            };
         }
 
         private void InitMatrix()
@@ -166,30 +291,75 @@ namespace SteamExtender
 
         private void InitArray()
         {
-            
-             Thread.Sleep(1000);
-             observableValues = new ChartValues<DateModel>
-             {
-                 new DateModel{ DateTime=System.DateTime.Now, Value = 5 }
+            Random rand = new Random();
+            Series2 = new SeriesCollection(dayConfig) {
+                 new LineSeries //In-game
+                 {
+                    Values = observableValues2.InGameValues,
+                    Fill = new SolidColorBrush(Color.FromRgb(60, 83, 48)),
+                    Stroke = new SolidColorBrush(Color.FromRgb(107, 186, 69)),
+                    PointGeometrySize = 15,
+                    PointForeround = new SolidColorBrush(Color.FromRgb(68, 95, 50)),
+                    //AlternativeStroke = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0)),
+                    StrokeThickness = 3
+                 },
+                 new LineSeries //Online
+                 {
+                     Values = observableValues2.OnlineValues,
+                     Stroke = new SolidColorBrush(Color.FromRgb(83, 164, 196)),
+                     Fill = new SolidColorBrush(Color.FromRgb(49, 85, 99)),
+                     PointGeometrySize = 15,
+                     PointForeround = new SolidColorBrush(Color.FromRgb(57, 95, 109)),
+                     StrokeThickness = 3
+                 },
+                 new LineSeries //Away
+                 {
+                     Values = observableValues2.AwayValues,
+                     Stroke = new SolidColorBrush(Color.FromRgb(61, 118, 141)),
+                     Fill = new SolidColorBrush(Color.FromRgb(44, 83, 98)),
+                     PointGeometrySize = 15,
+                     PointForeround = new SolidColorBrush(Color.FromRgb(57, 95, 109)),
+                     StrokeThickness = 3
+                 },
+                 new LineSeries //Snooze
+                 {
+                     Values = observableValues2.SnoozeValues,
+                     Stroke = new SolidColorBrush(Color.FromRgb(61, 118, 141)),
+                     Fill = new SolidColorBrush(Color.FromRgb(44, 83, 98)),
+                     PointGeometrySize = 15,
+                     PointForeround = new SolidColorBrush(Color.FromRgb(57, 95, 109)),
+                     StrokeThickness = 3
+                 },
+                 new LineSeries //Offline
+                 {
+                     Values = observableValues2.OfflineValues,
+                     Stroke = new SolidColorBrush(Color.FromRgb(106, 106, 106)),
+                     Fill = new SolidColorBrush(Color.FromRgb(55, 55, 55)),
+                     PointGeometrySize = 15,
+                     PointForeround = new SolidColorBrush(Color.FromRgb(63, 63, 63)),
+                     StrokeThickness = 3
+                 }
              };
-             Thread.Sleep(1000);
-             observableValues.Add(new DateModel { DateTime = System.DateTime.Now, Value = 6 });
-             Thread.Sleep(3000);
-             observableValues.Add(new DateModel { DateTime = System.DateTime.Now, Value = 1 });
-             Series = new SeriesCollection(dayConfig) { new StepLineSeries { Values = observableValues } };
+            observableValues2.AddValue(DateTime.Now, (SteamStatus)rand.Next(0, 4));
+            observableValues2.AddValue(DateTime.Now, (SteamStatus)rand.Next(0, 4));
+            observableValues2.AddValue(DateTime.Now, (SteamStatus)rand.Next(0, 4));
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            //observableValues.Add(new DateModel { DateTime = System.DateTime.Now, Value = 3 });
-            
-            
+            Random rand = new Random();
+            observableValues2.AddValue(DateTime.Now, (SteamStatus)rand.Next(0, 5));
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
             InitMatrix();
         }
 
         private void chart_MouseMove(object sender, MouseEventArgs e)
         {
             double offset;
+            label.Content = "Pixels: " + StepWidthInPixels + " Seconds: " + StepWidthInSeconds;
             if (e.LeftButton == MouseButtonState.Pressed || e.RightButton == MouseButtonState.Pressed)
             {
                 if (currMousePosition.X == -1 || currMousePosition.Y == -1)
@@ -223,6 +393,56 @@ namespace SteamExtender
             else
             {
                 FullResolution = FullResolution * Div;
+            }
+        }
+
+        /// <summary>
+        /// Логика при нажатии на одну из точек графика(открытие особенностей)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="chartPoint"></param>
+        private void chart_DataClick(object sender, ChartPoint chartPoint)
+        {
+            var asPixels = chart.ConvertToPixels(chartPoint.AsPoint());
+            MessageBox.Show("You clicked (" + chartPoint.X + ", " + chartPoint.Y + ") in pixels (" +
+                                        asPixels.X + ", " + asPixels.Y + ")");
+        }
+
+        /// <summary>
+        /// Возвращает индекс ближайшего по времени элемента(меньшего чем заданный в graphpt)
+        /// </summary>
+        /// <param name="series">Где искать</param>
+        /// <param name="graphpt">Время в Graph</param>
+        /// <returns></returns>
+        private int findClosestFitInSeries(ISeriesView series, long ticks) 
+        {
+            IEnumerable<DateModel> arr = series.Values.Cast<DateModel>();
+            int end = arr.Count() - 1;
+            int beg = 0;
+            int currMid = (end + beg) / 2;
+            while (true)
+            {
+                long currTick = arr.ElementAt(currMid).DateTime.Ticks;
+                if (currTick == ticks)
+                {
+                    return currMid;
+                }
+                else if (end - beg == 1) // максимальное приближение сейчас
+                {
+                    if (currTick > ticks)
+                        return currMid - 1;
+                    return currMid;
+                }
+                else if (currTick > ticks)
+                {
+                    end = currMid;
+                    currMid = (end + beg) / 2;
+                }
+                else
+                {
+                    beg = currMid;
+                    currMid = (end + beg) / 2;
+                }
             }
         }
     }
